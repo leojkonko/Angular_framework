3921cc3c9eafab30f402ef0aa4d7ff0458f9b7b4
leojkonko <leonardolinomeyer@gmail.com>
2023-09-01 10:13:52 -0300

    Visualizando resposta da requisição

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RequestLogin } from 'src/app/resources/models/RequestLogin';
+import { AlertService } from 'src/app/resources/services/alert.service';
 import { LoginService } from 'src/app/resources/services/login.service';
 
 @Component({
@@ -13,16 +14,18 @@ export class LoginComponent {
 
   public requestLogin: RequestLogin = new RequestLogin();
 
-  constructor(private loginService: LoginService){}
+  constructor(private loginService: LoginService, private alertService: AlertService){}
 
   public doLogin() :void{
     console.log(this.requestLogin);
     this.loginService.doLogin(this.requestLogin).subscribe(
-      data => {
-      console.log(data);
-    },
-    error => {
-      console.error();
+    data => {
+      this.alertService.info("",'Funcionalidade ainda não implementada!');
+      //console.log(data);
+    }, 
+    (httpError) => {
+      this.alertService.error("",httpError.error.message);
+      //console.error();
     })
   }
 }
